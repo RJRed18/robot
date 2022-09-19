ef731dcdf86744fa06dc8187834e3f3b08b03e62
RJRed18 <113944238+RJRed18@users.noreply.github.com>
2022-09-19 14:41:37 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+if (IR.IR_read() == 0 && 0 == 0) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+}
+basic.forever(function () {
+	
+})
